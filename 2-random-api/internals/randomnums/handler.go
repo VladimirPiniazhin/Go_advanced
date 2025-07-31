dab1965ecc4b5b72cd855e6202deec8a2d93a227
Vladimpi <vladimir.piniazhin@gmail.com>
2025-07-31 10:43:17 +0300

    Данные обновлены теперь все 3 задания в главном

---
+++ 2-random-api/internals/randomnums/handler.go
@@ -0,0 +1,21 @@
+package randomnums
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+)
+
+type RandomNumbersHandler struct{}
+
+func NewRandomNumbersHandler(router *http.ServeMux) {
+	handler := &RandomNumbersHandler{}
+	router.HandleFunc("/random", handler.Random())
+}
+
+func (handler *RandomNumbersHandler) Random() http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		random := rand.Intn(6) + 1
+		fmt.Fprintf(w, "%d", random)
+	}
+}
